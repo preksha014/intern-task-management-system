aa070896474ba2f141346ea9e9f9619ce80d0d69
Preksha <ranapreksha8@gmail.com>
2025-05-06 17:42:53 +0530

    feat(auth): add authentication middleware for user role management

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\RedirectIfAuthenticated;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            // 'auth' => \App\Http\Middleware\Authenticate::class,
-            // 'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            "auth"  => Authenticate::class,
+            "guest" => RedirectIfAuthenticated::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
